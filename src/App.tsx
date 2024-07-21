3b13ff69b9f202d0036e66d4cd9510f42384be5f
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-07-21 18:40:11 +0530

    JSX Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import DataTable from "./components/DataTable";
+import { data } from "./data";
 
 function App() {
   return (
-    <div className="w-full md:px-24 px-5">
-      <DataTable />
+    <div className="w-full md:px-24 px-5 py-8">
+      <DataTable
+        data={data}
+        searchBar
+        excelExport
+        pageSizeControl
+        pagination
+        removeRows
+      />
     </div>
   );
 }
--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -2,6 +2,15 @@ import { useMemo, useState } from "react";
 import * as XLSX from "xlsx";
 import { ColumnData, sortType } from "../../types";
 import { data } from "./../../data/index";
+import {
+  CaretDownOutline,
+  CaretUpOutline,
+  ChevronBackOutline,
+  ChevronForwardOutline,
+  DownloadOutline,
+  SearchOutline,
+  TrashOutline,
+} from "react-ionicons";
 
 interface DataTableProps {
   data: ColumnData;
@@ -129,7 +138,178 @@ const DataTable: React.FC<DataTableProps> = ({
 
   const totalPages = Math.ceil(filteredRows.length / pageSize);
 
-  return <div></div>;
+  return (
+    <div className="max-w-full overflow-x-auto ">
+      <div className="flex w-full items-center justify-between mb-5">
+        {searchBar ? (
+          <div className="flex w-[30%] items-center gap-5 rounded-lg px-3 py-2 bg-[#303030]">
+            <SearchOutline cssClasses={"!text-gray-300"} />
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="w-full outline-none bg-transparent"
+            />
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <div className="flex items-center gap-5">
+          {excelExport && (
+            <button
+              onClick={exportTableExcel}
+              className="rounded-lg bg-[#303030] p-2"
+            >
+              <DownloadOutline
+                width={"26px"}
+                height={"26px"}
+                cssClasses={"!text-[#99e5be] cursor-pointer"}
+              />
+            </button>
+          )}
+          {removeRows && (
+            <button
+              onClick={handleDaleteSelectedRows}
+              className="rounded-lg bg-[#303030] p-2 disabled:opacity-50"
+            >
+              <TrashOutline
+                width={"26px"}
+                height={"26px"}
+                cssClasses={`${
+                  selectedRows.length === 0
+                    ? "cursor-default !text-red-300"
+                    : "cursor-pointer !text-red-400"
+                }`}
+              />
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="table-container">
+        <table className="w-full overflow-x-auto max-w-[100vw]">
+          <thead>
+            <tr className="bg-[#303030] h-[50px]">
+              <th className="hidden">Actions</th>
+              <th className="font-medium text-gray-300 text-[16px] pl-5">#</th>
+              {columns.map((column, index: number) => (
+                <th
+                  key={index}
+                  onClick={() => handleSort(column)}
+                  className="font-medium text-gray-300 text-[16px] px-5 curso-pointer"
+                >
+                  <div className="flex items-center justify-center gap-[1px]">
+                    {column}
+                    {sortConfig?.key === column ? (
+                      sortConfig.direction === "asc" ? (
+                        <CaretUpOutline cssClasses={"!fill-blue-400"} />
+                      ) : sortConfig.direction === "desc" ? (
+                        <CaretDownOutline cssClasses={"!fill-blue-400"} />
+                      ) : (
+                        <CaretDownOutline cssClasses={"hidden"} />
+                      )
+                    ) : (
+                      <CaretDownOutline cssClasses={"hidden"} />
+                    )}
+                  </div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {paginatedRows.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className={`h-[50px] cursor-pointer ${selectedRows.includes(
+                  String(rowIndex)
+                    ? "bg-[#4d4d4d]"
+                    : rowIndex % 2
+                    ? "bg-[#242424]"
+                    : "bg-[#f1f1f1]"
+                )}`}
+              >
+                <td className="hidden">
+                  <input
+                    type="check"
+                    checked={selectedRows.includes(String(rowIndex))}
+                    onChange={() => handleRowSelect(rowIndex)}
+                  />
+                </td>
+                <td className="pl-5">{rowIndex + 1}</td>
+                {columns.map((column, index: number) => {
+                  const value = row[column];
+                  const columnData = data[column];
+                  const classNames = columnData.className
+                    ? columnData.className(value)
+                    : {};
+                  const content = columnData.renderValue
+                    ? columnData.renderValue(value)
+                    : typeof value === "boolean" && columnData.renderBoolean
+                    ? columndata.renderBoolean(value)
+                    : `${value}`;
+
+                  return (
+                    <td
+                      key={index}
+                      className={`${classNames}`}
+                      onClick={() => handleRowSelect(rowIndex)}
+                    >
+                      <div className="flex items-center justify-center">
+                        {content
+                          ? content
+                          : columnData.renderBoolean
+                          ? columnData.renderBoolean(value)
+                          : "false"}
+                      </div>
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="w-full mt-5 flex items-center justify-between">
+        {pageSizeControl ? (
+          <div className="rounded-lg py-[2px] pr-1 bg-[#303030] w-fit text-gray-400">
+            <select
+              value={pageSize}
+              onChange={(e) => setPageSize(Number(e.target.value))}
+              className="bg-transparent outline-none w-[80px] px-1"
+            >
+              <option value={5}>5</option>
+              <option value={10}>10</option>
+              <option value={15}>15</option>
+              <option value={20}>20</option>
+            </select>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {pagination && (
+          <div className="flex items-center gap-3">
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage === 0}
+              className="rounded-lg bg-[#303030] p-2 disabled:opacity-50"
+            >
+              <ChevronBackOutline cssClasses={"!text-gray-500"} />
+            </button>
+            <span className="text-sm">
+              Page {currentPage + 1} of {totalPages}
+            </span>
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage + 1 === 0}
+              className="rounded-lg bg-[#303030] p-2 disabled:opacity-50"
+            >
+              <ChevronForwardOutline cssClasses={"!text-gray-500"} />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DataTable;
