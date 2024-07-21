22ef2b17c8ea8150a3bc62d09ec10e5ca5cc1fbf
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-07-21 15:24:43 +0530

    Data Table Components

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import { ColumnData, sortType } from "../../types";
 
-const DataTable = () => {
+interface DataTableProps {
+  data: ColumnData;
+  searchBar?: boolean;
+  excelExport?: boolean;
+  pagination?: boolean;
+  removeRows?: boolean;
+  pageSizeControl?: boolean;
+}
+
+const DataTable: React.FC<DataTableProps> = ({
+  data,
+  searchBar = false,
+  excelExport = false,
+  pagination = false,
+  removeRows = false,
+  pageSizeControl = false,
+}) => {
+  const columns = Object.keys(data.columns);
+  const rowCount = Math.max(
+    ...columns.map((column) => data[column].values.length)
+  );
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [sortConfig, setSortConfig] = useState<sortType | null>(null);
+  const [pageSize, setPageSize] = useState(10);
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
   return <div></div>;
 };
 
